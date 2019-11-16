38661d070a843562b4ed0764ce4e9c489dadcb0f
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-11-16 21:23:02 +0200

    Created contacts with avatar, name and twitter handle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react'
-import ListContacts from './ListContact'
+import ListContacts from './ListContacts'
 
 const contacts = [
      {
        "id": "karen",
        "name": "Karen Isgrigg",
-       "handle": "karen_isgrigg",
+       "handle": "@karen_isgrigg",
        "avatarURL": "http://localhost:5001/karen.jpg"
      },
      {
        "id": "richard",
        "name": "Richard Kalehoff",
-       "handle": "richardkalehoff",
+       "handle": "@richardkalehoff",
        "avatarURL": "http://localhost:5001/richard.jpg"
      },
      {
       "id": "tyler",
       "name": "Tyler McGinnis",
-      "handle": "tylermcginnis",
+      "handle": "@tylermcginnis",
       "avatarURL": "http://localhost:5001/tyler.jpg"
     }
 ];
