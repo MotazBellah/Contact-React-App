db8dd854093473c685b806e8af882b5d2f7d5ecb
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-12-01 15:05:25 +0200

    used componentDidMount lifcycle to get the contacts from server and update the contacts in the state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 
 class App extends Component {
     state = {
-        contacts : [
-             {
-               "id": "karen",
-               "name": "Karen Isgrigg",
-               "handle": "@karen_isgrigg",
-               "avatarURL": "http://localhost:5001/karen.jpg"
-             },
-             {
-               "id": "richard",
-               "name": "Richard Kalehoff",
-               "handle": "@richardkalehoff",
-               "avatarURL": "http://localhost:5001/richard.jpg"
-             },
-             {
-              "id": "tyler",
-              "name": "Tyler McGinnis",
-              "handle": "@tylermcginnis",
-              "avatarURL": "http://localhost:5001/tyler.jpg"
-          },
-        ]
+        contacts: []
+    }
+    //Use componentDidMount to fetch data from server and update the contacts
+    componentDidMount(){
+        ContactsAPI.getAll()
+            .then((contacts) => {
+                this.setState(() => ({
+                    contacts
+                }))
+            })
     }
 
     removeContact = (contact) => {
